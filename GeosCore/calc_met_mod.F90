e3c3306e5eb36c811384890585ad504f625aa028
Liam Bindle <lrbindle@gmail.com>
2021-09-02 13:26:08 -0500

    Removed ALBEDO as a proxy for the land type.

--- GeosCore/calc_met_mod.F90
+++ GeosCore/calc_met_mod.F90
@@ -291,20 +291,14 @@ CONTAINS
        IsLocNoon(I,J) = ( LocTimeSec(I,J)          <= 43200  .and. &
                           LocTimeSec(I,J) + Dt_Sec >= 43200 )
 
-       ! Land: LWI=1 and ALBEDO less than 69.5%
-       State_Met%IsLand(I,J) = ( NINT( State_Met%LWI(I,J) ) == 1 .and. &
-                               State_Met%ALBD(I,J)  <  0.695e+0_fp )
+       ! Land snow
+       FRSNO = State_Met%FRSNO(I,J)
 
-       ! Water: LWI=0 and ALBEDO less than 69.5%
-       State_Met%IsWater(I,J) = ( NINT( State_Met%LWI(I,J) ) == 0 .and. &
-                                State_Met%ALBD(I,J)  <  0.695e+0_fp )
-
-       ! Ice: LWI=2 or ALBEDO > 69.5%
-       State_Met%IsIce(I,J) = ( NINT( State_Met%LWI(I,J) ) == 2 .or. &
-                              State_Met%ALBD(I,J)  >= 0.695e+0_fp )
-
-       ! Snow covered: ALBEDO > 40%
-       State_Met%IsSnow(I,J) = ( State_Met%ALBD(I,J)  > 0.40e+0_fp )
+       ! Set IsLand, IsWater, IsIce, IsSnow based on max fractional area
+       State_Met%IsLand(I,J)  = (FRLAND_NOSNO_NOICE > MAX(FRWATER, FRICE, FRSNO))
+       State_Met%IsWater(I,J) = (FRWATER > MAX(FRLAND_NOSNO_NOICE, FRICE, FRSNO))
+       State_Met%IsIce(I,J)   = (FRICE > MAX(FRLAND_NOSNO_NOICE, FRWATER, FRSNO))
+       State_Met%IsSnow(I,J)  = (FRSNO > MAX(FRLAND_NOSNO_NOICE, FRWATER, FRICE))
 
     ENDDO
     ENDDO
